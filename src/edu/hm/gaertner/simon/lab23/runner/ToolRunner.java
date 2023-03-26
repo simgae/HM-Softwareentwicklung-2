f4db11ab7fa874a0f131d888e8cc95cd20fe0c97
Simon Gärtner <simongaertner@gmx.net>
2023-03-26 09:42:18 +0200

    Revert changes in .gitignore file

---
+++ src/edu/hm/gaertner/simon/lab23/runner/ToolRunner.java
@@ -0,0 +1,28 @@
+package edu.hm.gaertner.simon.lab23.runner;
+import edu.hm.cs.rs.se.ss23.tool.BaseToolRunner;
+
+import java.io.IOException;
+
+/** Beispielprogramm zum Start der Werkzeugkette.
+ * Praktikum SE2, SS2023 (Schiedermeier).
+ * @version 2023-03-19
+ */
+public class ToolRunner extends BaseToolRunner {
+    @Override public String projectDir() {
+        return "C:\\Users\\User\\Desktop\\SE2\\sgaertne";
+    }
+
+    @Override public String toolBundleDir() {
+        return "C:\\Users\\User\\Desktop\\SE2\\tool";
+    }
+
+    /** Hauptprogramm.
+     * Startet nacheinander die Werkzeuge und sichert die Ergebnisse im Filesystem.
+     * @param args Kommandozeilenargument: Name eines Subpackage einer Aufgabe.
+     *             Beispiel "a00" fuer die Probeaufgabe.
+     * @throws IOException wenn der Zugriff aufs Filesystem scheitert.
+     */
+    public static void main(String... args) throws IOException {
+        new ToolRunner().runAll(args[0]);
+    }
+}
