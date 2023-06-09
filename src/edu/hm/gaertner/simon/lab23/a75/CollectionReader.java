fe1cc43b39baa3fa7c0607d9fa5685df9f6d2c76
Simon Gärtner <simongaertner@gmx.net>
2023-06-09 11:51:41 +0200

    Cleanup

--- src/edu/hm/gaertner/simon/lab23/a75/CollectionReader.java
+++ src/edu/hm/gaertner/simon/lab23/a75/CollectionReader.java
@@ -1,8 +1,6 @@
 package edu.hm.gaertner.simon.lab23.a75;
 
-import java.io.FileWriter;
-import java.io.IOException;
-import java.io.Reader;
+import java.io.*;
 import java.util.Collection;
 import java.util.Iterator;
 import java.util.List;
@@ -18,14 +16,20 @@ public class CollectionReader extends Reader{
     @Override
     public int read(char[] cbuf, int off, int len) throws IOException {
 
-        int index = 0;
+        int index = off;
+        int copyOff = off;
 
-        while (off < len && iterator.hasNext()){
-            cbuf[index] = iterator.next();
-            index++;
+        while (index < len && iterator.hasNext()){
+            if(index >= copyOff){
+                cbuf[index] = iterator.next();
+                index++;
+            } else{
+                copyOff--;
+                iterator.next();
+            }
         }
 
-        return index;
+        return index - off;
     }
 
     @Override
@@ -33,15 +37,12 @@ public class CollectionReader extends Reader{
     }
 
     public static void main(String... args) throws IOException {
-        try(FileWriter writer = new FileWriter("abc.txt");
-            Reader reader = new CollectionReader(List.of('a', 'b', 'c'))) {
+        try(Reader reader = new CollectionReader(List.of('a', 'b', 'c'))) {
 
             char[] letters = new char[3];
 
-            System.out.println(reader.read(letters, 0 , 3));
+            System.out.println(reader.read(letters, 0, 3));
             System.out.println(letters);
-
-            // reader.transferTo(writer);
         }
     }
 }
